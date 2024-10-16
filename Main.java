883ee02ad88654a0d12304f91ac9cedf489034b5
Joy Gacheri <joydouglas011@gmail.com>
2024-10-16 16:17:38 +0300

    classes

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public  static void main(String[] args) {
+        Person p = new Person();
+        p.setName("John");
+        p.setAge(20);
+        p.display();
+    }
+
+}
